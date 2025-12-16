3a909cc7348b56a698a54f085504b494463c4d5e
kishansinghifs1 <kishansingh956196@gmai.com>
2025-12-17 01:35:49 +0530

    feat: introduce frontend UI for CI/CD demo and add `/api/joke` endpoint to fetch jokes.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const https = require("https");
 
 const app = express();
 const PORT = 3000;
@@ -12,6 +13,28 @@ app.get("/api/health", (req, res) => {
     service: "Jenkins CI/CD Demo",
     version: "v1.0",
     timestamp: new Date()
+  });
+});
+
+app.get("/api/joke", (req, res) => {
+  const url = "https://v2.jokeapi.dev/joke/Any";
+
+  https.get(url, (apiRes) => {
+    let data = "";
+    apiRes.on("data", (chunk) => {
+      data += chunk;
+    });
+
+    apiRes.on("end", () => {
+      try {
+        const parsedData = JSON.parse(data);
+        res.json(parsedData);
+      } catch (error) {
+        res.status(500).json({ error: "Failed to parse external API response" });
+      }
+    });
+  }).on("error", (err) => {
+    res.status(500).json({ error: "Error fetching data from external API", details: err.message });
   });
 });
 
